804c49e9114670de054b628cd5a8ca49d7488e7d
riveria94 <timelesschaos94@gmail.com>
2017-02-07 19:07:42 +0800

    Extended Address, Email and Phone.java to Contact.java

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,27 +6,26 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address {
+public class Address extends Contact {
 
     public static final String EXAMPLE = "123, some street";
     public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
     public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
-    public final String value;
-    private boolean isPrivate;
+    public Contact data = new Contact();
 
-    /**
+	/**
      * Validates given address.
      *
      * @throws IllegalValueException if given address string is invalid.
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
         String trimmedAddress = address.trim();
-        this.isPrivate = isPrivate;
+        this.data.isPrivate = isPrivate;
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
-        this.value = trimmedAddress;
+        this.data.value = trimmedAddress;
     }
 
     /**
@@ -38,22 +37,22 @@ public class Address {
 
     @Override
     public String toString() {
-        return value;
+        return data.value;
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Address // instanceof handles nulls
-                && this.value.equals(((Address) other).value)); // state check
+                && this.data.value.equals(((Address) other).data.value)); // state check
     }
 
     @Override
     public int hashCode() {
-        return value.hashCode();
+        return data.value.hashCode();
     }
 
     public boolean isPrivate() {
-        return isPrivate;
+        return data.isPrivate;
     }
 }
